bd1d88e7d756f5201b32d0dcbfa5f69f0abcf3e0
josephgu <josephgu0323@outlook.com>
2024-10-28 21:26:51 +0800

    blog init

--- src/config.ts
+++ src/config.ts
@@ -5,42 +5,42 @@ export const SiteLanguage = 'zh';
 export const SiteTitle = '无尽光芒';
 export const SiteDescription = '踏歌长行，梦想永在';
 export const FooterDescription = '肆意闪耀';
-export const AdminName = 'Ladit';
+export const AdminName = 'Joseph Gu';
 export const PageSize = 15;
 
 // socialPlatform => userName
 // check components/Header.astro socialConfig for more info
 export const Socials: Record<string, Record<string, string>> = {
-	mail: { url: 'mailto:i@ladit.me' },
-	github: { url: 'https://github.com/ladit/astro-blog-zozo' },
-	x: { url: 'https://x.com/userName' },
-	mastodon: { url: 'https://mastodon.social/@userName' },
-	facebook: { url: 'https://facebook.com/userName' },
-	instagram: { url: 'https://instagram.com/userName' },
-	telegram: { url: 'https://t.me/@userName' },
-	youtube: { url: 'https://youtube.com/@userName' },
-	skype: { url: 'https://skype.com/userName' },
-	slack: { url: 'https://slack.com/userName' },
-	messenger: { url: 'https://messenger.com/userName' },
-	whatsapp: { url: 'https://whatsapp.com/userName' },
-	snapchat: { url: 'https://snapchat.com/userName' },
-	line: { url: 'https://line.com/userName' },
-	twitch: { url: 'https://twitch.com/userName' },
-	weibo: { url: 'https://weibo.com/userName' },
-	bilibili: { url: 'https://space.bilibili.com/userName' },
-	dingding: { url: 'https://dingtalk.com/userName' },
-	zhihu: { url: 'https://zhihu.com/userName' },
-	douban: { url: 'https://douban.com/userName' },
+	mail: { url: 'josephgu0323@outlook.com' },
+	github: { url: 'https://github.com/josephgu41' },
+	x: { url: 'https://x.com/gu_no41' },
+	// mastodon: { url: 'https://mastodon.social/@userName' },
+	// facebook: { url: 'https://facebook.com/userName' },
+	// instagram: { url: 'https://instagram.com/userName' },
+	// telegram: { url: 'https://t.me/@userName' },
+	// youtube: { url: 'https://youtube.com/@userName' },
+	// skype: { url: 'https://skype.com/userName' },
+	// slack: { url: 'https://slack.com/userName' },
+	// messenger: { url: 'https://messenger.com/userName' },
+	// whatsapp: { url: 'https://whatsapp.com/userName' },
+	// snapchat: { url: 'https://snapchat.com/userName' },
+	// line: { url: 'https://line.com/userName' },
+	// twitch: { url: 'https://twitch.com/userName' },
+	// weibo: { url: 'https://weibo.com/userName' },
+	bilibili: { url: 'https://space.bilibili.com/241667860' },
+	// dingding: { url: 'https://dingtalk.com/userName' },
+	// zhihu: { url: 'https://zhihu.com/userName' },
+	// douban: { url: 'https://douban.com/userName' },
 	rss: { url: '/rss.xml' },
 };
 
 // doc: https://giscus.app
 // data-theme is auto changed between noborder_light / noborder_gray
 export const GiscusConfig: Record<string, string> = {
-	'data-repo': 'ladit/astro-blog-zozo',
-	'data-repo-id': 'R_kgDOLgobXQ',
+	'data-repo': 'josephgu41/blog',
+	'data-repo-id': 'R_kgDONCT8Zg',
 	'data-category': 'Announcements',
-	'data-category-id': 'DIC_kwDOLgobXc4Cd_N6',
+	'data-category-id': 'DIC_kwDONCT8Zs4Cjwiz',
 	'data-mapping': 'pathname',
 	'data-strict': '0',
 	'data-reactions-enabled': '1',
